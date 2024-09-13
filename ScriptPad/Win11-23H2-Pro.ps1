a2789c0fd61038b9634c9c6cc68173f6d91c548c
bownet-hub <bowby1@gmail.com>
2024-09-13 13:22:02 -0400

    Update Win11-23H2-Pro.ps1

--- ScriptPad/Win11-23H2-Pro.ps1
+++ ScriptPad/Win11-23H2-Pro.ps1
@@ -5,25 +5,23 @@ $OSActivation = 'Retail'
 $OSLanguage = 'en-us'
 
 #Set OSDCloud Vars
+Write-Host -ForegroundColor Green “Starting OSDCloud ZTI”
 $Global:StartOSDCloud = [ordered]@{
-    #Restart = [bool]$False
+    Restart = [bool]$true
     RecoveryPartition = [bool]$true
-    #OEMActivation = [bool]$True
     WindowsUpdate = [bool]$true
     WindowsUpdateDrivers = [bool]$true
-    #WindowsDefenderUpdate = [bool]$true
     SetTimeZone = [bool]$true
     ClearDiskConfirm = [bool]$false
-    #ShutdownSetupComplete = [bool]$false
-    #SyncMSUpCatDriverUSB = [bool]$true
-    #CheckSHA1 = [bool]$true
 }
+
+get-variable startosdcloud -valueonly
 
 #Write-Host -ForegroundColor Green “Starting OSDCloud ZTI”
 #Start-Sleep -Seconds 5
 
 Write-Host -ForegroundColor Green “Importing OSD PowerShell Module”
-Import-Module OSD -Force
+#Import-Module OSD -Force
 
 Write-Host -ForegroundColor Green “Start OSDCloud”
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+#Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
