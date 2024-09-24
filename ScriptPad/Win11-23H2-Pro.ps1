82a5cc4e734f750e4ac845f2db7a6182aea017f5
bownet-hub <bowby1@gmail.com>
2024-09-24 14:59:38 -0400

    Update Win11-23H2-Pro.ps1

--- ScriptPad/Win11-23H2-Pro.ps1
+++ ScriptPad/Win11-23H2-Pro.ps1
@@ -7,6 +7,10 @@ $OSLanguage = 'en-us'
 #Set OSDCloud Vars
 Write-Host -ForegroundColor Green “Setting variables”
 $Global:MyOSDCloud = [ordered]@{
+    OSName = 'Windows 11 23H2 x64'
+    OSEdition = 'Pro'
+    OSActivation = 'Retail'
+    OSLanguage = 'en-us'
     Restart = [bool]$true
     ZTI = [bool]$true
     }
@@ -15,4 +19,5 @@ Get-Variable MyOSDCloud -ValueOnly
 Start-Sleep -Seconds 5
 
 Write-Host -ForegroundColor Green “Starting OSDCloud”
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+#Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+Start-OSDCloud -ZTI
