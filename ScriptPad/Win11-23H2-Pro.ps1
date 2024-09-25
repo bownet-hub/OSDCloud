1845f0f7ce27a2103858e785729910d1dc8901b2
bownet-hub <bowby1@gmail.com>
2024-09-25 16:41:57 -0400

    Update Win11-23H2-Pro.ps1

--- ScriptPad/Win11-23H2-Pro.ps1
+++ ScriptPad/Win11-23H2-Pro.ps1
@@ -1,8 +1,8 @@
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
-#$OSName = 'Windows 11 23H2 x64'
-#$OSEdition = 'Pro'
-#$OSActivation = 'Retail'
-#$OSLanguage = 'en-us'
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-us'
 
 #Set OSDCloud Vars
 Write-Host -ForegroundColor Green “Setting variables”
@@ -19,5 +19,5 @@ Get-Variable MyOSDCloud -ValueOnly
 Start-Sleep -Seconds 5
 
 Write-Host -ForegroundColor Green “Starting OSDCloud”
-#Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
-Start-OSDCloud -ZTI
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+#Start-OSDCloud -ZTI
