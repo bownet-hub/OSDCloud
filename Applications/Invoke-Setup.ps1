6407b3c5a64943fad1797e4a1f9e6f335efd6bd9
bownet-hub <bowby1@gmail.com>
2024-09-30 11:38:12 -0400

    Update Invoke-Setup.ps1

--- Applications/Invoke-Setup.ps1
+++ Applications/Invoke-Setup.ps1
@@ -39,13 +39,13 @@ If ($AppName -eq "RingCentral") {
         )
 
         Install
-        }
+    }
     
     Elseif ($Invoke -eq "Uninstall") {
         Write-Host "Uninstalling $AppName"
         Uninstall-Advanced
-        }
     }
+}
 
 #WatchGuard SSL VPN
 Elseif ($AppName -eq "Watchguard") {
@@ -60,11 +60,16 @@ Elseif ($AppName -eq "Watchguard") {
        "/VERYSILENT"
         )
 
+        $CertFile = ".\OpenVPN.cer"
+        If (!(Test-Path "$CertFile")) { 
+            Write-Host "Certificate not present"
+        } else {
         Write-Host "Install included certificate"
-        Import-Certificate -FilePath .\OpenVPN.cer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+        Import-Certificate -FilePath $CertFile -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
 
         Install
         }
+    }
     
     Elseif ($Invoke -eq "Uninstall") {
         Write-Host "Uninstalling $AppName"
@@ -75,8 +80,8 @@ Elseif ($AppName -eq "Watchguard") {
         "/NORESTART"
         )
         Uninstall
-        }
     }
+}
 
 Else {
     Write-Host "$AppName is not a valid application"
@@ -92,22 +97,26 @@ If ($url) {
     If (!(Test-Path "$InstallerPath")) {
         Write-Host "Did not download, Exit Script"
         exit
-        }
     }
+}
 
-Write-Host "Starting install using $Installer $Arguments"
-Start-Process "$Installer" -ArgumentList $Arguments -Wait
-Write-Host "Software install finished" 
+If (!(Test-Path "$Installer")) { 
+    Write-Host "Installer not present"
+} else {
+    Write-Host "Starting install using $Installer $Arguments"
+    Start-Process "$Installer" -ArgumentList $Arguments -Wait
+    Write-Host "Software install finished" 
+}
 
 #Remove the Installer
 
-If ($InstallerPath -ne $null -and ($InstallerPath -ne $null)) {
+If ($null -ne $InstallerPath -and ($null -ne $InstallerPath)) {
     If (Test-Path "$InstallerPath") {
         Start-Sleep -Seconds 2
         Write-Host "Delete installer"
         Remove-Item "$InstallerPath"
-        }
     }
+}
 Write-Host "$(Get-Date -Format $dtFormat)"
 }
 
@@ -118,7 +127,7 @@ If (!(Test-Path "$Installer")) {
     Write-Host "File does not exist"
 } elseif (Test-Path "$Installer") {
     Start-Process "$Installer" -ArgumentList $Arguments -Wait   
-    }
+}
 
 Write-Host "Software uninstall finished" 
 
@@ -135,11 +144,11 @@ Foreach ($app in (Get-WmiObject -Class Win32_Product | Where-Object{$_.Vendor -l
     Write-Host "Attempting to uninstall $($app)"
     Try {
         $app.Uninstall() | Out-Null 
-        } 
+    } 
     Catch {
         Write-Host "$_"
-        }
     }
+}
 
 Write-Host "Remove any system uninstall keys after trying the uninstaller"
 $paths = @("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", 
@@ -156,37 +165,37 @@ Foreach($path in $paths) {
                     If ($cmd -notlike "*/X*") { 
                         Write-Host "No /X flag - this isn't for uninstalling"
                         $cmd = "" 
-                        } #don't do anything if it's not an uninstall
+                    } #don't do anything if it's not an uninstall
                     Elseif ($cmd -notlike "*/qn*") { 
                         Write-Host "Adding /qn flag to try and uninstall quietly"
                         $cmd = "$cmd /qn" 
-                        } #don't display UI
-                    }
+                    } #don't display UI
+                }
                 If ($cmd) {
                     Write-Host "Executing $($cmd)"
                     cmd.exe /c "$($cmd)"
                     Write-Host "Done"
-                    }
-                }
-            Catch {
-                Write-Host "$_"
                 }
             }
+            Catch {
+                Write-Host "$_"
+            }
+        }
         $list = Get-ItemProperty "$path\*" | Where-Object {$_.DisplayName -like "*$AppName*"} | Select-Object -Property PSPath
         Foreach($regkey in $list) {
             Write-Host "Removing Registry Key $($regkey.PSpath)"
             Try {
                 Remove-Item $regkey.PSPath -recurse -force
-                } 
+            } 
             Catch {
                 Write-Host "$_"
-                }
             }
-        } 
+        }
+    } 
     Else { 
     Write-Host "Path $($path) not found" 
-        }
     }
+}
 Write-Host "Uninstall complete"
 Write-Host "$(Get-Date -Format $dtFormat)"
 }
