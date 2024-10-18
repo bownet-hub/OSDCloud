776f0a453accac2fd962d292132327b0f1db5e8b
bownet-hub <bowby1@gmail.com>
2024-10-18 12:39:14 -0400

    Update OSDCloudGUI.ps1

--- ScriptPad/OSDCloudGUI.ps1
+++ ScriptPad/OSDCloudGUI.ps1
@@ -1,2 +1,28 @@
+# Set OSDCloudGUI Defaults
+$Global:OSDCloud_Defaults = [ordered]@{
+    BrandName            = "Out of the Box Solutions"
+    BrandColor           = "Blue"
+    OSActivation         = "Retail"
+    OSEdition            = "Pro"
+    OSLanguage           = "en-us"
+    OSImageIndex         = 9
+    OSName               = "Windows 11 24H2 x64"
+    OSReleaseID          = "24H2"
+    OSVersion            = "Windows 11"
+    captureScreenshots   = $false
+    ClearDiskConfirm     = $false
+    restartComputer      = $true
+    updateDiskDrivers    = $false
+    updateFirmware       = $false
+    updateNetworkDrivers = $false
+    updateSCSIDrivers    = $false
+}
+
+# Create 'Start-OSDCloudGUI.json' - During WinPE SystemDrive will be 'X:'
+$OSDCloudGUIjson = New-Item -Path "$($env:SystemDrive)\OSDCloud\Automate\Start-OSDCloudGUI.json" -Force
+
+# Covert data to Json and export to the file created above
+$Global:OSDCloud_Defaults | ConvertTo-Json -Depth 10 | Out-File -FilePath $($OSDCloudGUIjson.FullName) -Force
+
 Write-Host -ForegroundColor Green “Starting OSDCloud”
-Start-OSDCloudGUI -BrandName "Out of the Box Solution"
+Start-OSDCloudGUI
