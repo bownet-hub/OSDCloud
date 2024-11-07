d98734f49e4dc37067604e30cd569f0b971c8e11
bownet-hub <bowby1@gmail.com>
2024-11-07 13:16:34 -0500

    Update Get-APDiags.ps1

--- Applications/Get-APDiags.ps1
+++ Applications/Get-APDiags.ps1
@@ -8,5 +8,17 @@ param(
     [Parameter(Mandatory = $false)] [string]$AppId,
     [Parameter(Mandatory = $false)] [string]$AppSecret
 )
+$RegistryKey = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Autopilot\EnrollmentStatusTracking"
+$RegistryWatcher = New-Object System.Management.ManagementEventWatcher
+$RegistryWatcher.Query = New-Object System.Management.WqlEventQuery("__InstanceModificationEvent", "TargetInstance isa 'RegistryKey' and TargetInstance.Name = '$RegistryKey'")
 
-Get-AutopilotDiagnosticsCommunity.ps1 @PSBoundParameters
+Register-ObjectEvent $RegistryWatcher -Action {
+    Write-Host "Registry change detected at $(Get-Date)"
+    Write-Host "Key Path: $($Event.SourceEventArgs.NewEvent.TargetInstance.Name)"
+    Get-AutopilotDiagnosticsCommunity.ps1 @PSBoundParameters
+}
+
+# Keep the script running to monitor changes
+while ($true) {
+    Start-Sleep -Seconds 1
+}
